b5b11276fd105dd17b2a96604ef6cbb6d35ab11c
Kykung <59070071@it.kmitl.ac.th>
2020-09-23 09:00:39 +0700

    Add an endpoint for getting a specific card

--- cards/views.py
+++ cards/views.py
@@ -3,6 +3,7 @@ from cards.serializers import CardSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
 
 class CardList(APIView):
     def get(self, request, format=None):
@@ -16,3 +17,16 @@ class CardList(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class CardDetail(APIView):
+    def get_object(self, pk):
+        try:
+            card = Card.objects.get(pk=pk)
+            return card
+        except Card.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        card = self.get_object(pk)
+        serializer = CardSerializer(card)
+        return Response(serializer.data)
