cdb1e4d2b1ac5a5e2c340e359f183ed42b352b2b
Kykung <59070071@it.kmitl.ac.th>
2020-09-23 08:54:02 +0700

    Add an endpoint for adding new card

--- cards/views.py
+++ cards/views.py
@@ -2,9 +2,17 @@ from cards.models import Card
 from cards.serializers import CardSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 class CardList(APIView):
     def get(self, request, format=None):
         cards = Card.objects.all()
         serializer = CardSerializer(cards, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        data = request.data
+        serializer = CardSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
